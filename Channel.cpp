e90baeaca93664a7586a71112bb5c81471aeaecb
luchen11 <zhangzhao0608@gmail.com>
2017-04-08 18:46:59 +0800

    完善Channel class

--- Channel.cpp
+++ Channel.cpp
@@ -1,3 +1,99 @@
 #include "Channel.h"
 
+const int Channel::READEVENT = EPOLLIN | EPOLLPRI;
+const int Channel::NONEEVENt = 0;
+const int Channel::WRITEEVENT = EPOLLOUT;
+
+Channel::Channel(EventLoop *loop, int fd): 
+  loop_(loop), fd_(fd), events_(0), status_(-1)
+{
+}
+
+void Channel::setReadCallBack(std::function<void()> callBack)
+{
+  readCallBack_ = callBack;
+}
+
+void Channel::setWriteCallBack(std::function<void()> callBack)
+{
+  writeCallBack_ = callBack;
+}
+
+void Channel::setErrorCallBack(std::function<void()> callBack)
+{
+  errorCallBack_ = callBack;
+}
+
+void Channel::_update()
+{
+  loop_->updateChannel(this);
+}
+
+void Channel::remove()
+{
+  loop_->removeChannel(this);
+}
+
+void Channel::disableReading()
+{
+  events_ &= (~READEVENT); 
+  _update();
+}
+
+void Channel::enableReading()
+{
+  events_ |= READEVENT;
+  _update();
+}
+
+void Channel::disableWriting()
+{
+  events_ &= (~WRITEEVENT);
+  _update();
+}
+
+void Channel::enableWriting()
+{
+  events_ |= WRITEEVENT;
+  _update();
+}
+
+void disableAll()
+{
+  events_ &= NONEEVENt;
+}
+
+bool Channel::isWriting()
+{
+  return events_ & WRITEEVENT;
+}
+
+bool Channel::isReading()
+{
+  return events_ & READEVENT;
+}
+
+void Channel::handleEvent()
+{
+  if(respondEvents_ & EPOLLHUP && (!respondEvents_ & EPOLLIN))
+  {
+    if(errorCallBack_) errorCallBack_();
+  }
+
+  if(respondEvents_ & (EPOLLIN | EPOLLPRI | EPOLLHUP))
+  {
+    if(readCallBack_) readCallBack_();
+  }
+
+  if(respondEvents_ & ( EPOLLERR | EPOLLVAL))
+  {
+    if(errorCallBack_) errorCallBack_();
+  }
+
+  if(respondEvents_ & EPOLLOUT)
+  {
+    if(writeCallBack_) writeCallBack_();
+  }
+}
+
 
--- Channel.h
+++ Channel.h
@@ -1,13 +1,85 @@
 #ifndef CHANNEL_H
 #define CHANNEL_H
+#include <functional>
 
 #include "EventLoop.h"
 
 class Channel
 {
+  public:
+    Channel(EventLoop *loop, int fd);
+
+    void handleEvent();
+
+    void setReadCallBack(std::function<void()> callBack);
+    void setWriteCallBack(std::function<void()> callBack);
+    void setErrorCallBack(std::function<void()> callBack);
+    void SetCloseCallBack(std::function<void()> callBack);
+
+    void remove();
+
+    int fd() const
+    {
+      return fd_;
+    }
+
+    void setRespondEvents(int event)
+    {
+      respondEvents_ = event;
+    }
+
+    int events()
+    {
+      return events_;
+    }
+
+    EventLoop* ownerLoop() const
+    {
+      return loop_;
+    }
+
+    void disableReading();
+    void enableReading();
+
+    void disableWriting();
+    void enableWriting();
+
+    void disableAll();
+
+    bool isWriting();
+    bool isReading();
+
   private:
-    EventLoop *loop;
-    int state;
+
+    static const int READEVENT;
+    static const int WRITEEVENT;
+    static const int NONEEVENt;
+    void _update();
+
+    EventLoop *loop_;
+
+    //表示当前Channel的状态
+    //-1: 初始化
+    // 0: 已添加
+    // 1：已删除
+    int status_;
+    // enum status_ {
+    //  Snew, Sadd, Sdelete};
+
+    const int fd_;
+
+    //本身关注的事件
+    int events_;
+
+    //epoll 响应的事件
+    int respondEvents_;
+
+    std::function<void()> readCallBack_;
+    std::function<void()> writeCallBack_;
+    std::function<void()> closeCallBack_;
+    std::function<void()> errorCallBack_;
+    std::function<void()> eventCallBack_;
+
 }
 
-#endif
+
