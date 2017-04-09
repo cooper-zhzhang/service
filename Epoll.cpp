39d6951b96b748dc6a1c850b13ec0f651eb6af22
luchen11 <zhangzhao0608@gmail.com>
2017-04-09 21:30:59 +0800

    完善EventLoop, 功能实现完毕

--- Epoll.cpp
+++ Epoll.cpp
@@ -1,1 +1,80 @@
 #include "Epoll.h"
+#include <sys/epoll.h>
+#include "Channel.h"
+#include <vector>
+#include "EventLoop"
+
+Epoll::Epoll(EventLoop *loop)
+  : loop_(loop), epollfd_(::epoll_create1(EPOLL_CLOEXEC))
+{
+}
+
+Epoll::~Epoll()
+{
+  ::close(epollfd_);
+}
+
+void Epoll::removeChannel(Channel *channel)
+{
+  int fd = channel->fd();
+  channels_.erase(fd);
+
+
+  if(channel->status() == ADDRED)
+    _update(EPOLL_CTL_DEL, channel);
+
+  channel->setStatus(Channel::NEWED);
+
+}
+
+void Epoll::updateChannel(Channel *channel)
+{
+  int status = channel->status();
+
+  if(status == Channel::ADDRED)
+  {
+    if(channel->isNonEvent())
+      _update(EPOLL_CTL_DEL, channel);
+    else
+      _update(EPOLL_CTL_MOD, channel);
+  }
+
+  else if(status == Channel::DELETED)
+  {
+    channel->setStatus(Channel::ADDRED);
+    _update(EPOLL_CTL_ADD, channel);
+  }
+
+  else if(status == Channel::NEWED)
+  {
+    channels_[fd] = channel;
+    _update(EPOLL_CTL_ADD, channel);
+    channel->setStatus(Channel::ADDRED);
+  }
+
+}
+
+void Epoll::_update(int operation, Channel *channel)
+{
+  struct epoll_event event;
+  bzero(event, sizeof(event));
+  event.data.ptr = channel;
+  event.events = channel->events();
+
+  int fd = channel->fd();
+
+  //TODO 添加错误回滚
+  epoll_ctl(epollfd_, operation, fd, &event);
+}
+
+void Epoll::fillActiveChannel(int num, std::vector<Channel*> *channelList)
+{
+  for(int i = 0; i < num; ++i)
+  {
+    Channel *channel = static_cast<Channel*>(events_[i].data.ptr);
+    channel->setRespondEvents(events_[i].events);
+    channelList->push_back(channel);
+  }
+}
+
+
--- Epoll.h
+++ Epoll.h
@@ -2,6 +2,8 @@
 #define EPOLL_H
 
 #include <map>
+#include <vector>
+#include <sys/epoll.h>
 #include "EventLoop.h"
 #include "Channel.h"
 
@@ -9,14 +11,16 @@ class Epoll
 {
   public:
     Epoll(EventLoop *loop);
-    addChannel();
-    deleteChannel();
-    updateChannel();
+    void removeChannel(Channel *channel);
+    void updateChannel(Channel *channel);
+    void poll(int timeOut);
 
   private:
-    void update(int operation, Channel *channel);
+    void fillActiveChannel(int num, std::vector<Channel*> *chanbelList);
+    void _update(int operation, Channel *channel);
     int epollfd_;
     std::map<int, Channel*> channels_;
-    EventLoop *loop;
-}
+    EventLoop *loop_;
+    std::vector<struct epoll_event> events_;
+};
 #endif
