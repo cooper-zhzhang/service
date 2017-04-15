ad896194819c1ca2363212530b83e68c13470272
luchen11 <zhangzhao0608@gmail.com>
2017-04-15 19:41:13 +0800

    完成TcpServer class

--- TcpServer.cpp
+++ TcpServer.cpp
@@ -1,3 +1,65 @@
 #include "TcpServer.h"
+#include "Socket.h"
+
+TcpServer::TcpServer(EventLoop *loop, InetAddress &serverAddres, std::string name)
+  :loop_(loop), name_(name), acceptor_(new Acceptor(loop, serverAddres)),
+  connectionCallBack_(defaultConnectionCallBack()),
+  messageCallback_(defaultMessageCallBack()),
+  nextConnId_(1),
+  threadPools_(new EventLoopThreadPool(loop, name))
+{
+  start_.store(0);
+  acceptor_->newConnectionCallBack(std::bind(&TcpServer::newConnection, this, _1, _2));
+}
+
+TcpServer::~TcpServer()
+{
+  for(auto it = connections_.begin(); it != connections_.end(); ++it)
+  {
+    auto conn = it->second;
+    //std::shared_ptr<Connection> conn = it->second;
+    it->second.reset();
+    conn->getLoop()->runInLoop(std::bind(&TcpConnection::stop, conn));
+    conn->reset();
+  }
+}
+
+void TcpServer::start()
+{
+  if(start_.load() == 0)
+  {
+    threadPools_->start();
+    loop->runInLoop(std::bind(&Acceptor::linsten, acceptor_.get()));
+  }
+}
+
+void TcpServer::_newConnection(int sockfd, InetAddress &clientAddres)
+{
+  EventLoop* loop = threadPools_->getNextEvent();
+  std::string connName = name_ + std::to_string(++nextConnId_);
+
+  InetAddress serAddres(Socket::getLocalAddres(sockfd));
+
+  std::shared_ptr<TcpConnection> conn (new TcpConnection(loop, connName, sockfd, serAddres, clientAddres));
+
+  connections_[connName] = conn;
+  conn->setConnectionCallBack(connectionCallBack_);
+  conn->setMessageCallBack(messageCallback_);
+  conn->setCloseCallBack(std:bind(&TcpServer::_removeConnection, this, _1));
+  loop->runInLoop(std::bind(&TcpConnection::start, conn));
+}
+
+
+void TcpServer::_removeConnection(const std::shared_ptr<TcpConnection> &connection)
+{
+  loop_->runInLoop(std::bind(&TcpServer::_removeConnectionInLoop, this, connection));
+}
+
+void TcpServer::_removeConnectionInLoop(const std::shared_ptr<TcpConnection> &connection)
+{
+  connections_.erase(conn->name());
+  EventLoop *loop = conn->getLoop();
+  loop->runInQueue(std::bind(&TcpConnection::stop, conn));
+}
 
 
--- TcpServer.h
+++ TcpServer.h
@@ -4,6 +4,7 @@
 #include <string>
 #include "EventLoop.h"
 #include "InetAddress.h"
+#include "Buffer.h"
 
 
 class TcpServer
@@ -16,7 +17,22 @@ class TcpServer
 
     void start();
 
-    void setMessageCallBack(std::function<void(const TcpConnectionPtr&)> callBack)
+    EventLoop* getEventLoop()
+    {
+      return loop_;
+    }
+
+    void setThreadNum(int num)
+    {
+      threadPools_->setThreadNum(num);
+    }
+
+    std::string name()
+    {
+      return name_;
+    }
+
+    void setMessageCallBack(std::function<void(const TcpConnectionPtr&, Buffer*)> callBack)
     {
       messageCallback_ = callBack;
     }
@@ -26,11 +42,15 @@ class TcpServer
       connectionCallBack_ = callBack;
     }
 
-privaet:
-    void newConnection(int sockfd, InetAddress &clientAddres);
-    void removeConnection(const std::shared_ptr<TcpConnection> &connection);
-    EventLoop *loop_;
+private:
     const std::string name_;
+
+    void _newConnection(int sockfd, InetAddress &clientAddres);
+    void _removeConnection(const std::shared_ptr<TcpConnection> &connection);
+    void _removeConnectionInLoop(const std::shared_ptr<TcpConnection> &connection);
+    EventLoop *loop_;
+    int nextConnId_;
+
     std::unique_ptr<Acceptor> acceptor_;
     std::shared_ptr<EventLoopThreadPool> threadPools_;
 
@@ -38,6 +58,9 @@ privaet:
     std::function<void (const std::shared_ptr<TcpConnection> &)> connectionCallBack_;
 
     std::map<std::string, std::shared_ptr<TcpConnection>> connections_;
+
+    //使用原子性,防止重复
+    std::atomic<int> start_;
 
 };
 #endif
