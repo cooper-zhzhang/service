c7bcd86515adcb55f28a63b23ecee88be787ce6f
luchen11 <zhangzhao0608@gmail.com>
2017-04-16 17:41:34 +0800

    测试cmakelist

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include "Thread.h"
+#include <iostream>
+#include <string>
+#include <functional>
+#include <pthread.h>
+
+
+void fun()
+{
+  std::cout << "this is a thread" << std::endl;
+}
+
+int main()
+{
+
+  std::function<void()> thread_fun = fun;
+  Thread thread(thread_fun, std::string("test"));
+  std::cout << thread.name() << std::endl;
+  thread.start();
+  thread.join();
+
+  return 0;
+}
