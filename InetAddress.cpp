df803a7998c908a6b5074d7148ed66501b1d176a
luchen11 <zhangzhao0608@gmail.com>
2017-04-18 16:14:35 +0800

    格式化代码

--- InetAddress.cpp
+++ InetAddress.cpp
@@ -3,22 +3,23 @@
 #include <cstring>
 #include <netinet/in.h>
 
-InetAddress::InetAddress(int port)
+InetAddress::InetAddress(uint16_t port)
 {
-  bzero(&address_, sizeof(struct sockaddr_in));
-  address_.sin_family = AF_INET;
-  address_.sin_addr.s_addr = htonl(INADDR_ANY);
-  address_.sin_port = htons(port);
+    bzero(&address_, sizeof(struct sockaddr_in));
+    address_.sin_family = AF_INET;
+    address_.sin_addr.s_addr = (INADDR_ANY);
+    inet_pton(AF_INET, "0.0.0.0", &(address_.sin_addr));
+
+    address_.sin_port = htons(port);
 }
 
 sa_family_t InetAddress::family() const
 {
-  return address_.sin_family;
+    return address_.sin_family;
 }
 
 int InetAddress::port() const
 {
-  return ntohs(address_.sin_port);
-
+    return ntohs(address_.sin_port);
 }
 
