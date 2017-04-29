9da63cc1fe5256d55b91ee2851ab4b915360266a
luchen11 <zhangzhao0608@gmail.com>
2017-04-29 23:22:53 +0800

    fix

--- pic.cpp
+++ pic.cpp
@@ -1,17 +1,39 @@
 #include <unistd.h>
-#include <fctln.h>
 #include <iostream>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <strings.h>
+#include <arpa/inet.h>
 
 int main(int argc, char **argv)
 {
   int image = open(argv[1], O_RDONLY);
+  if(image == -1)
+  {
+    std::cout << "open error" << std::endl;
+    exit(-1);
+  }
   int fd = socket(AF_INET, SOCK_STREAM, 0);
-  struct sockaddr_in;
-  sockaddr_in.sin_family = AF_INET;
-  sockaddr_in.sin_addr.s_addr = (INADDR_ANY);
-  sockaddr_in.sin_port = htons(3000);
-  connect(fd, &sockaddr_in, sizeof(sockaddr_in));
-  void buffer[100];
+  if(fd == -1)
+  {
+    std::cout << "socket error" << std::endl;
+    exit(-1);
+  }
+
+  struct sockaddr_in addres;
+  bzero(&addres, sizeof(addres));
+  addres.sin_family = AF_INET;
+  addres.sin_addr.s_addr = (INADDR_ANY);
+  addres.sin_port = htons(3000);
+
+  int result = connect(fd, (sockaddr*)&addres, sizeof(addres));
+  if(result == -1)
+  {
+    std::cout << "connect error" << std::endl;
+  }
+
+  char buffer[100];
   int num;
   while((num = read(image, buffer, 100)) != 0)
   {
